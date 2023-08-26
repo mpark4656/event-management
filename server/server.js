7813f5f88880769949c04dc71ff401ce75482315
mpark4656 <mpark4656@gmail.com>
2023-08-26 02:08:04 -0400

    Pass wildcard route in Express to the React Router

--- server/server.js
+++ server/server.js
@@ -7,9 +7,14 @@ const app = express();
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
 	res.send('Hello World!');
 });
+
+// Send index html file from React and let the react router handle this
+app.get('*', (req, res) => {
+	res.sendFile('index.html', {root: 'dist/'});
+})
 
 app.listen(process.env.PORT, () => {
 	console.log(`server started on port ${process.env.PORT}`);
